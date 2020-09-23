beb4f12618c9b1ee3614d4ea229569e4d06a73a4
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-23 19:41:21 -0300

    create the create-transaction service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,57 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Invalid value for transaction type.');
+    }
+
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+
+      if (balance.total - value < 0) {
+        throw new AppError('Not enough funds to complete transaction.');
+      }
+    }
+
+    let categoryEntity = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryEntity) {
+      categoryEntity = categoriesRepository.create({ title: category });
+      await categoriesRepository.save(categoryEntity);
+    }
+
+    const newTransaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryEntity.id,
+    });
+    await transactionsRepository.save(newTransaction);
+
+    return newTransaction;
   }
 }
 
